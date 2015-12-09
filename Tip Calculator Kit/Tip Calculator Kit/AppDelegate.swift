ba86eb56d32dbf3d9606b25e42e1fa0a04c15a33
Kitwana Akil <kakil@itelinc.com>
2015-12-09 08:53:45 -0500

    Initial commit of the Tip Calculator app challenge

---
+++ Tip Calculator Kit/Tip Calculator Kit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tip Calculator Kit
+//
+//  Created by Kitwana on 12/9/15.
+//  Copyright © 2015 AkilDev LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tip Calculator Kit/Tip Calculator Kit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tip Calculator Kit
+//
+//  Created by Kitwana on 12/9/15.
+//  Copyright © 2015 AkilDev LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
